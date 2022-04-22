0c342e9565c7b96248d55bdab86e0baf60bb1e10
Abu Saleh Muhammad Shaon <srabon.php@gmail.com>
2022-04-22 15:49:22 +0600

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '3.0.6',
+        'version' => '3.0.6.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'codeitnowin/barcode',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'codeitnowin/barcode' => array(
+            'pretty_version' => '3.0.6',
+            'version' => '3.0.6.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
